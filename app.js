4f633cb58a336fa7449cfbe8532b773b6f97dd39
william-samwakajr <williamsamwaka05@gmail.com>
2024-01-23 15:09:09 +0200

    added button functionality

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 let taskToAdd = document.getElementById('add input');
-let addButton = document.getElementById('create-task');
+const addButton = document.getElementById('create-task');
+let taskarea = document.getElementsByClassName("tasks");
 
 function addTask() {
+    let newTask = document.createElement('p');
+    newTask.innerHTML = taskToAdd.value;
 
+    taskarea.append(newTask);
+    taskToAdd.value = '';
 }
 
 function deleteTask() {
 
 }
+
+addButton.addEventListener('click', addTask)
